837069345c05e796c8c4f9a1525f88687fb6722e
Max Caduff <max.caduff@heig-vd.ch>
2018-06-05 02:32:29 +0200

    sticky sessions done, need to modify step 6

--- docker-images/3-reverse-proxy/changeAddresses.sh
+++ docker-images/3-reverse-proxy/changeAddresses.sh
@@ -2,14 +2,16 @@
 # this script should be called with 2 or more arguments, the first being the dynamic container's ip and the next ones being the statics container's ip. (should include ports.)
 
 
-echo "<Proxy balancer://staticCluster>
-" > txt
+echo 'Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
+<Proxy balancer://staticCluster>
+' > txt
 for ((i=2;i<=$#;i++));
 do
-  eval echo "    BalancerMember http://\$$i" >> txt
+  eval echo "	BalancerMember http://\$$i route=\$(($i-1))" >> txt
 done
 
 echo "
+	ProxySet stickysession=ROUTEID
 </Proxy>
 
 <VirtualHost *:80>
